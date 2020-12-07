63acf537c1632e959f331252b2daa918617c0ada
Mateusz <37753289+Evil00787@users.noreply.github.com>
2020-12-07 22:07:45 +0100

    Added graph creation

---
+++ sequencing/sequencing.py
@@ -0,0 +1,20 @@
+import networkx as nx
+
+def _create_graph(spectrum, l):
+	G = nx.MultiDiGraph()
+	vertices = list(set(map(lambda x: x[:-1], spectrum)))
+	arr = []
+	for el in spectrum:
+		for vert in vertices:
+			for x in range(l-1, -1, -1):
+				if x == 0:
+					arr.append((el[:-1], vert, {'weight': x, 'value': el[-1]}))
+				elif el[l-x:] == vert[:x]:
+					arr.append((el[:-1], vert, {'weight': x, 'value': el[-1]}))
+	G.add_edges_from(arr)
+	print(list(G.edges(data=True)))
+	return G
+
+if __name__ == '__main__':
+	spectrum, l = ["AAA", "AAC", "ACA", "CAC", "CAA", "ACG", "CGC", "GCA", "ACT", "CTT", "TTA", "TAA"], 3
+	graph = _create_graph(spectrum, l)
