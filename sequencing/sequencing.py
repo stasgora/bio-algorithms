d7b99d2f1a6c527dcb9be099df549ef3f0a1fcc6
Mateusz <37753289+Evil00787@users.noreply.github.com>
2021-01-13 17:50:45 +0100

    Now fully working with negative and positive errors!

--- sequencing/sequencing.py
+++ sequencing/sequencing.py
@@ -6,7 +6,7 @@ INITIAL = 'initial'
 
 max_depth = 0
 max_profit = 0
-
+positive_errors = 0
 
 def _create_graph(spect, l):
 	G = nx.MultiDiGraph()
@@ -22,10 +22,10 @@ def _create_graph(spect, l):
 					isInSpec = 0
 				for x in range(l-1, -1, -1):
 					if x == 0:
-						arr.append((el[:-1], vertTo, {PROFIT: x - isInSpec, VALUE: el[-1]}))
+						arr.append((el[:-1], vertTo, {PROFIT: max(x - isInSpec, 0), VALUE: el[-1], INITIAL: isInSpec == 0}))
 						break
 					elif el[l-x:] == vertTo[:x]:
-						arr.append((el[:-1], vertTo, {PROFIT: x - isInSpec, VALUE: el[-1]}))
+						arr.append((el[:-1], vertTo, {PROFIT: x - isInSpec, VALUE: el[-1], INITIAL: isInSpec == 0}))
 						break
 	G.add_edges_from(arr)
 	print("--------------GRAPH--------------")
@@ -33,32 +33,47 @@ def _create_graph(spect, l):
 	return G
 
 
-def _find_euler(G, neg_errors):
+def _find_euler(G, spectrum, neg_errors):
 	arr = []
 	print("--------------EULER--------------")
 	for val in G.nodes:
 		copy = G
-		arr = _check_node(copy.copy(), val, max_depth, [val], neg_errors)
+		arr = _check_node(copy.copy(), val, max_depth, [val], spectrum, neg_errors)
 		if type(arr) is list:
 			break
 	return arr
 
 
-def _check_node(copy, node, depth, routes, neg_errors_left):
+def _check_node(copy, node, depth, routes, spectrum, neg_errors_left):
 	print("    " * (max_depth - depth) + "Checking " + node + "'s edges")
 	for routeNode in copy[node]:
 		for route in copy[node][routeNode]:
-			if depth == 1:
-				print("Finished checking! Returning results...")
-				lastVal = copy[node][routeNode][route][VALUE]
-				return routes + [lastVal,  routes[-1][-1] + lastVal]
-			if neg_errors_left + copy[node][routeNode][route][PROFIT] - max_profit >= 0:
-				print("    " * (max_depth - depth) + "Going to " + routeNode + " by " + copy[node][routeNode][route][VALUE] + " with profit " + str(copy[node][routeNode][route][PROFIT]))
-				ret = _check_node(_remove_edges(copy, node, copy[node][routeNode][route][VALUE]), routeNode, depth - 1, routes + [copy[node][routeNode][route][VALUE], routeNode], neg_errors_left + copy[node][routeNode][route][PROFIT] - max_profit)
-				if type(ret) is not bool:
-					return ret
-				else:
-					print("    " * (max_depth - depth) + "Bad route :( Moving back....")
+			for i in range(max_profit, -1, -1):
+				if depth == 1:
+					if neg_errors_left + copy[node][routeNode][route][PROFIT] - max_profit == 0:
+						print("Finished checking! Returning results...")
+						spec_copy = spectrum[:]
+						if copy[node][routeNode][route][INITIAL]:
+							spec_copy.remove(node + copy[node][routeNode][route][VALUE])
+						if len(spec_copy) == positive_errors:
+							lastVal = copy[node][routeNode][route][VALUE]
+							return routes + [lastVal, copy[node][routeNode][route][PROFIT], routes[-1][-1] + lastVal]
+					elif copy[node][routeNode][route][INITIAL]:
+						spec_copy = spectrum[:]
+						spec_copy.remove(node + copy[node][routeNode][route][VALUE])
+						if len(spec_copy) == positive_errors:
+							lastVal = copy[node][routeNode][route][VALUE]
+							return routes + [lastVal, max_profit - neg_errors_left, routes[-1][-1] + lastVal]
+				elif neg_errors_left + copy[node][routeNode][route][PROFIT] - max_profit >= 0 and copy[node][routeNode][route][PROFIT] == i:
+					print("    " * (max_depth - depth) + "Going to " + routeNode + " by " + copy[node][routeNode][route][VALUE] + " with profit " + str(copy[node][routeNode][route][PROFIT]))
+					spec_copy = spectrum[:]
+					if copy[node][routeNode][route][INITIAL]:
+						spec_copy.remove(node + copy[node][routeNode][route][VALUE])
+					ret = _check_node(_remove_edges(copy, node, copy[node][routeNode][route][VALUE]), routeNode, depth - 1, routes + [copy[node][routeNode][route][VALUE], copy[node][routeNode][route][PROFIT], routeNode], spec_copy, neg_errors_left + copy[node][routeNode][route][PROFIT] - max_profit)
+					if type(ret) is not bool:
+						return ret
+					else:
+						print("    " * (max_depth - depth) + "Bad route :( Moving back....")
 	print("    " * (max_depth - depth) + "Really bad node!!! Moving back harder.....")
 	return False
 
@@ -111,10 +126,10 @@ def _make_spectrum_nice_with_errors(arr, length):
 
 
 if __name__ == '__main__':
-	positive_errors = 0
+	positive_errors = 1
 	negative_errors = 1
-	spectrum = ["AAA", "AAT", "AAC", "ACA", "CAC", "CAA", "CGC", "GCA", "ACT", "CTT", "TTA", "TAA"]
-	# spectrum = ["PPPPPPct", "PPPPPcta", "PPPPctaa", "PPPctaaG", "PPctaaGG", "PctaaGGT", "ctaaGGTC", "taaGGTCC", "aaGGTCCC", "aGGTCCCT", "GGTCCCTe", "GTCCCTea"]
+	spectrum = ["AAA", "GTA", "CGC", "ACA", "CAC", "ACG", "CAA", "GCA", "ACT", "TTA", "CTT", "TAA"]
+	# spectrum = ["AAAAAACT", "AAAAACTA", "AAAACTAA", "AAACTAAG", "AACTAAGG", "ACTAAGGT", "CTAAGGTC", "TAAGGTCC", "AAGGTCCC", "AGGTCCCT", "GGTCCCTG", "GTCCCTGA"]
 	# spectrum = ["ACG", "CCG", "CGA", "CGT", "GAC"]
 
 	max_depth = len(spectrum) - positive_errors
@@ -129,5 +144,5 @@ if __name__ == '__main__':
 			break
 	if not stop:
 		graph = _create_graph(spectrum, length)
-		_make_spectrum_nice_no_errors(_find_euler(graph, negative_errors), length)
+		_make_spectrum_nice_with_errors(_find_euler(graph, spectrum, negative_errors), length)
 
